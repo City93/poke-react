b5640f5a3f066857c0ae84d8fd1f4141660c029c
Christian Ciudad <88143659+City93@users.noreply.github.com>
2021-12-23 08:56:38 +0100

    Add files via upload

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import './Footer.css'
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <footer>All rights reserved to Pokemon™ </footer>;
 };
 
 export default Footer;
